a412aca58a93dd75573f4c5b7fac2e43dc4871e3
sidney durant <sidney.durant@gmail.com>
2025-05-05 19:59:59 -0700

    Fix build issue

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,4 @@
 // This component is a sidebar that lets you change settings for the map display
-import React from 'react';
 
 // TODO: split sidebar into various components. Add a legend with units when any particular option is selected
 // TODO: figure out how best to do this. Add an infobubble.
